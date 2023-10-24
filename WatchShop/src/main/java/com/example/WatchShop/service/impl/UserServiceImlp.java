3d82866ce72a54955eabf4f745d92b4dc6712536
manhdung111297 <117447779+manhdung111297@users.noreply.github.com>
2023-10-25 00:23:07 +0700

    dungdev update CRUD Users

--- WatchShop/src/main/java/com/example/WatchShop/service/impl/UserServiceImlp.java
+++ WatchShop/src/main/java/com/example/WatchShop/service/impl/UserServiceImlp.java
@@ -6,9 +6,19 @@ import com.example.WatchShop.model.dto.UsersDTO;
 import com.example.WatchShop.repository.RolesRepository;
 import com.example.WatchShop.repository.UsersReponsitory;
 import com.example.WatchShop.service.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Optional;
 
 
 @Service
@@ -23,17 +33,24 @@ public class UserServiceImlp implements UserService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
 
     @Override
-    public Users getUsers(String email, String password) {
-        System.err.println(email + " email");
-        Users user = usersReponsitory.findByEmail(email);
-        if (user != null && encoder.matches(password, user.getPassword())) {
+    public Optional<Users> getUsersByEmailAndPassword(String email, String password) {
+        Optional<Users> user = usersReponsitory.findByEmail(email);
+        if (user.isPresent() && encoder.matches(password, user.get().getPassword())) {
             return user;
         }
         return null;
     }
-
 
     @Override
     public void addUsers(UsersDTO usersDTO) {
@@ -53,9 +70,71 @@ public class UserServiceImlp implements UserService {
     }
 
     @Override
+    public Users updateUsers(UsersDTO usersDTO, Long id) {
+        Optional<Users> optionalUsers = usersReponsitory.findById(id);
+        if (optionalUsers.isPresent()) {
+            Users users = optionalUsers.get();
+            users.setFullName(usersDTO.getFullName());
+            users.setBirthDate(usersDTO.getBirthDate());
+            users.setAddress(usersDTO.getAddress());
+            users.setPassword(encoder.encode(usersDTO.getPassword()));
+
+            // update email if does not exists
+            if (!usersReponsitory.existsByEmail(usersDTO.getEmail())) {
+                users.setEmail(usersDTO.getEmail());
+            }
+
+            users.setPhone(usersDTO.getPhone());
+            usersReponsitory.save(users);
+
+            return users;
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        usersReponsitory.deleteById(id);
+    }
+
+    @Override
+    public Users save(Users users) {
+        return usersReponsitory.save(users);
+    }
+
+    @Override
+    public Optional<Users> getUserByEmail(String email) {
+        return usersReponsitory.findByEmail(email);
+    }
+
+    @Override
     public boolean existsByEmail(String email) {
         return usersReponsitory.existsByEmail(email);
     }
+
+    // Send email
+    public void sendEmail(Users user, String subject, String emailContent) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(user.getEmail());
+        helper.setSubject(subject);
+        helper.setText(emailContent, true);
+        javaMailSender.send(message);
+    }
+
+    @Override
+    public void sendRecoverPassword(Users user, String password) throws MessagingException {
+        Context context = new Context();
+        context.setVariable("username", user.getFullName());
+        context.setVariable("password", password);
+        String emailContent = templateEngine.process("recovery_password", context);
+        sendEmail(user, "Forgot password", emailContent);
+
+    }
+
+
+
+
 
 
 }
---
+++ WatchShop/src/main/java/com/example/WatchShop/util/PasswordUtils.java
@@ -0,0 +1,36 @@
+package com.example.WatchShop.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class PasswordUtils {
+    public static String generateRandomPassword(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            SecureRandom random = SecureRandom.getInstanceStrong();
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            while (sb.length() < length) {
+                byte[] randomBytes = new byte[16];
+                random.nextBytes(randomBytes);
+                byte[] hashBytes = md.digest(randomBytes);
+
+                for (byte b : hashBytes) {
+                    if (characters.contains(String.valueOf((char) b))) {
+                        sb.append((char) b);
+
+                        if (sb.length() == length) {
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }}
