8cc46bde9d405212754ef1b47dd5c68caaebd463
Viet Dev <trancongviet0710@gmail.com>
2023-10-29 12:18:16 +0700

    data script

---
+++ WatchShop/SQL/rating.sql
@@ -0,0 +1,1 @@
+INSERT INTO watchshop.dbo.rating (id, create_date, update_date, id_product, id_user, star) VALUES (6, null, null, 1, 1, 5);
---
+++ WatchShop/SQL/roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO watchshop.dbo.roles (id, name) VALUES (1, N'ROLE_USER');
+INSERT INTO watchshop.dbo.roles (id, name) VALUES (2, N'ROLE_ADMIN');
