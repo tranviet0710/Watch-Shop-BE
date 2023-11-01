df575c1f9565367cb018702353dc546bbbc40db4
Viet Dev <trancongviet0710@gmail.com>
2023-11-02 00:12:00 +0700

    ỏders

--- WatchShop/src/main/java/com/example/WatchShop/model/Orders.java
+++ WatchShop/src/main/java/com/example/WatchShop/model/Orders.java
@@ -3,10 +3,7 @@ package com.example.WatchShop.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Date;
 import java.util.Set;
