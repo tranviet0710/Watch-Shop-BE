0d80d5ac7040771e449044e59917db27e529fbcb
Viet Dev <trancongviet0710@gmail.com>
2023-11-07 22:17:21 +0700

    top-5-customers

--- WatchShop/src/main/java/com/example/WatchShop/controller/statistic/StatisticController.java
+++ WatchShop/src/main/java/com/example/WatchShop/controller/statistic/StatisticController.java
@@ -1,8 +1,8 @@
 package com.example.WatchShop.controller.statistic;
 
-import com.example.WatchShop.model.Products;
-import com.example.WatchShop.model.dto.res.ProductDetailResDTO;
 import com.example.WatchShop.model.dto.res.ProductStatisticalResDTO;
+import com.example.WatchShop.model.dto.res.UserResDTO;
+import com.example.WatchShop.service.i_service.OrderService;
 import com.example.WatchShop.service.i_service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,11 +21,21 @@ public class StatisticController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/top-5-best-sellers")
-    public ResponseEntity<?> get5BestSellers(){
-        List<ProductStatisticalResDTO> get5BestSellers = productService.findAllProduct().stream()
+    public ResponseEntity<?> get5BestSellers() {
+        List<ProductStatisticalResDTO> bestSellers = productService.findAllProduct().stream()
                 .map(ProductStatisticalResDTO::new)
                 .sorted(Comparator.comparing(ProductStatisticalResDTO::getQuantitySold, Comparator.reverseOrder())).limit(5).toList();
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("status", "success", "data", get5BestSellers));
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("status", "success", "data", bestSellers));
+    }
+
+    @GetMapping("/top-5-best-customers")
+    public ResponseEntity<?> get5BestCustomer() {
+        List<UserResDTO> bestCustomers = orderService.get5BestSellers();
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("status", "success", "data", bestCustomers));
     }
 }
