bac28c0831b45f38633c28b89db3a7a883338ecc
minhtri2510 <minhtricgv@gmail.com>
2023-11-17 09:37:32 +0700

    add product and image

--- WatchShop/SQL/images.sql
+++ WatchShop/SQL/images.sql
@@ -1,33 +1,75 @@
-<<<<<<< HEAD
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-24', N'image1.jpg', N'2023-10-24', 1);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-25', N'image2.jpg', N'2023-10-25', 1);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-26', N'image3.jpg', N'2023-10-26', 2);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-27', N'image4.jpg', N'2023-10-27', 2);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-28', N'image5.jpg', N'2023-10-28', 2);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-29', N'image6.jpg', N'2023-10-29', 7);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-30', N'image7.jpg', N'2023-10-30', 7);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-10-31', N'image8.jpg', N'2023-10-31', 10);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-11-01', N'image9.jpg', N'2023-11-01', 11);
-INSERT INTO watchshop.dbo.images (create_date, source, update_date, id_product) VALUES (N'2023-11-02', N'image10.jpg', N'2023-11-02', 11);
-=======
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (4, N'2023-10-24', N'image1.jpg', N'2023-10-24', 1);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (5, N'2023-10-25', N'image2.jpg', N'2023-10-25', 1);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (6, N'2023-10-26', N'image3.jpg', N'2023-10-26', 2);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (7, N'2023-10-27', N'image4.jpg', N'2023-10-27', 2);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (8, N'2023-10-28', N'image5.jpg', N'2023-10-28', 2);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (9, N'2023-10-29', N'image6.jpg', N'2023-10-29', 7);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (10, N'2023-10-30', N'image7.jpg', N'2023-10-30', 7);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (11, N'2023-10-31', N'image8.jpg', N'2023-10-31', 10);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (12, N'2023-11-01', N'image9.jpg', N'2023-11-01', 11);
-INSERT INTO watchshop.dbo.images (id, create_date, source, update_date, id_product)
-VALUES (13, N'2023-11-02', N'image10.jpg', N'2023-11-02', 11);
->>>>>>> 52f9f4d7885ec1df940876569ff19fd527145928
+
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'844b4415ed5d4d03bfeeefc7e205ac691700049723094.jpg', CAST(N'2023-11-15' AS Date), 1)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'49024a8a1d1245c4b63b3fb3dce0fd681700049834098.jpg', CAST(N'2023-11-15' AS Date), 2)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'fb0cfb9e7a02467f8d59b67cb2edf6801700049927690.jpg', CAST(N'2023-11-15' AS Date), 3)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'f42dcb0f595b46caa57316ff3b564a351700050066830.jpg', CAST(N'2023-11-15' AS Date), 4)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'a5c9ae7623c845e6b9e60066988b39251700050201599.jpg', CAST(N'2023-11-15' AS Date), 5)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'86d4dbfb122642be8a434f13f1b3d4b71700050418149.jpg', CAST(N'2023-11-15' AS Date), 6)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'c3c358ee7cf54e1da4374a6ec9c4636c1700066978184.jpg', CAST(N'2023-11-15' AS Date), 7)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'040a5c0abb8a4b4582a4f24c960b4c0f1700067101540.jpg', CAST(N'2023-11-15' AS Date), 8)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES (CAST(N'2023-11-15' AS Date), N'ea068502e34d41c5b47ceb91ae122eed1700067198366.jpg', CAST(N'2023-11-15' AS Date), 9)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-15' AS Date), N'3193bf160bf0425dbb1a2f9143c6e2ee1700067265699.jpg', CAST(N'2023-11-15' AS Date), 10)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-15' AS Date), N'3fecf9d2c34849bf9c90ca37a763110c1700067415668.jpg', CAST(N'2023-11-15' AS Date), 11)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-15' AS Date), N'a87c8e50f9f8445a929b60f83d6f1b981700067546712.jpg', CAST(N'2023-11-15' AS Date), 12)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'369b8f9d6a2a4ef99a83a7daa3d656771700067703861.jpg', CAST(N'2023-11-16' AS Date), 13)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'd1f01fc6f6384656b5eef74b8ae3d01c1700067786530.jpg', CAST(N'2023-11-16' AS Date), 14)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'7cc1883d7cf14cada727d16711a6029e1700068029215.jpg', CAST(N'2023-11-16' AS Date), 15)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'36df9be92c084eb0b7f34d6be36c26121700068129781.jpg', CAST(N'2023-11-16' AS Date), 16)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'31b3546472344477bd5c2fac208492f81700068280150.jpg', CAST(N'2023-11-16' AS Date), 17)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'fcef688b581941f2a60e72d92d36f2151700068358356.jpg', CAST(N'2023-11-16' AS Date), 18)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'16811d57de2c4698ab95a1f5695c982f1700068472241.jpg', CAST(N'2023-11-16' AS Date), 19)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-16' AS Date), N'fe641be727f8445b8fd8b3011f1fe4721700068558417.jpg', CAST(N'2023-11-16' AS Date), 20)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'6d312a31a4654d80b039e442ee4d77551700186154829.jpg', CAST(N'2023-11-17' AS Date), 21)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'405db35fa47b48b79979786848bd004d1700186295058.jpg', CAST(N'2023-11-17' AS Date), 22)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'b24e744268c6446198a1f818ee50a30e1700186372031.jpg', CAST(N'2023-11-17' AS Date), 23)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'241a870c2b2a436cae80224efc5281c91700186546220.jpg', CAST(N'2023-11-17' AS Date), 24)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'4ebe7f01f38a4059b960046a4626fd701700186628649.jpg', CAST(N'2023-11-17' AS Date), 25)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'5e7dc456367b4099a7093123a35df9581700186716676.jpg', CAST(N'2023-11-17' AS Date), 26)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'1e61000912f64ebc8e8af267b80d46d11700186763462.jpg', CAST(N'2023-11-17' AS Date), 27)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'668979e71b484b758fb04b40e8fab6271700186898272.jpg', CAST(N'2023-11-17' AS Date), 28)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'13079960514240ddb74027fca87ec1961700186988848.jpg', CAST(N'2023-11-17' AS Date), 29)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'092947958ab14b90b83d4406a9df36ea1700187088442.jpg', CAST(N'2023-11-17' AS Date), 30)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'c3b7c4fde15647a8acb835c4e19e3d261700187181995.jpg', CAST(N'2023-11-17' AS Date), 31)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'9ef998bb09584575be3f3e9f407585e71700187376613.jpg', CAST(N'2023-11-17' AS Date), 32)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'f95d6d4c23a24574a1869389629338221700187602772.jpg', CAST(N'2023-11-17' AS Date), 33)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'4c478552172241279d6fafc486523b211700187691755.jpg', CAST(N'2023-11-17' AS Date), 34)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'8d60926cddb144ba87977c3264e42def1700187777065.jpg', CAST(N'2023-11-17' AS Date), 35)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'02417973da3d4c35969f75b8aee8e1801700187853654.jpg', CAST(N'2023-11-17' AS Date), 36)
+GO
+INSERT [dbo].[images] ( [create_date], [source], [update_date], [id_product]) VALUES ( CAST(N'2023-11-17' AS Date), N'd453b763b951413c81442a11826e57f71700187924709.jpg', CAST(N'2023-11-17' AS Date), 37)
+GO
